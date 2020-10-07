22e40477878155c1e3d973fc6756adb119a9f630
dkorbey <demirkanbaglamac@gmail.com>
2020-10-07 10:47:06 +0200

    bug fixed

--- Labs/03-gpio/gpio_library/gpio.h
+++
@@ -1,6 +0,0 @@
-#ifndef GPIO_H_        // Preprocessor directive allows for conditional compilation. If not defined.
-#define GPIO_H_         // Definition of constant within your source code.
-
-// The body of entire header file
-
-#endif                          // The #ifndef directive must be closed by an #endif
