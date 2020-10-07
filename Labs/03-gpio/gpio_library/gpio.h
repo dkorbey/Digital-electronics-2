52eed0a5c3c06a33d526a98f84dfa545953ae919
dkorbey <demirkanbaglamac@gmail.com>
2020-10-07 10:43:50 +0200

    Lab 3 files added

---
+++ Labs/03-gpio/gpio_library/gpio.h
@@ -0,0 +1,6 @@
+#ifndef GPIO_H_        // Preprocessor directive allows for conditional compilation. If not defined.
+#define GPIO_H_         // Definition of constant within your source code.
+
+// The body of entire header file
+
+#endif                          // The #ifndef directive must be closed by an #endif
---
+++ Labs/03-gpio/gpio_library/gpio_library/main.c
@@ -0,0 +1,60 @@
+/***********************************************************************
+ * 
+ * Alternately toggle two LEDs when a push button is pressed. Use 
+ * functions from GPIO library.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2019-2020 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+/* Defines -----------------------------------------------------------*/
+#define LED_GREEN   PB5     // AVR pin where green LED is connected
+#define LED_RED     PC0
+#define BTN			PD0
+#define BLINK_DELAY 500
+#ifndef F_CPU
+#define F_CPU 16000000      // CPU frequency in Hz required for delay
+#endif
+
+/* Includes ----------------------------------------------------------*/
+#include <util/delay.h>     // Functions for busy-wait delay loops
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include "gpio.h"           // GPIO library for AVR-GCC
+
+/* Function definitions ----------------------------------------------*/
+/**
+ * Main function where the program execution begins. Toggle two LEDs 
+ * when a push button is pressed. Functions from user-defined GPIO
+ * library is used instead of low-level logic operations.
+ */
+int main(void)
+{
+    /* GREEN LED */
+    GPIO_config_output(&DDRB, LED_GREEN);
+    GPIO_write_low(&PORTB, LED_GREEN); //LED off, because active-high
+
+    /* second LED */
+    GPIO_config_output(&DDRC, LED_RED);
+	GPIO_write_high(&PORTC, LED_RED); //LED off, because active-low
+	
+    /* push button */
+    GPIO_config_input_pullup(&DDRD, BTN);
+
+    // Infinite loop
+    while(1)
+    {
+        // Pause several milliseconds
+        _delay_ms(BLINK_DELAY);
+
+        // WRITE YOUR CODE HERE
+		GPIO_toggle(&PORTB, LED_GREEN);
+		
+		GPIO_toggle(&PORTC, LED_RED);
+    }
+
+    // Will never reach this
+    return 0;
+}
